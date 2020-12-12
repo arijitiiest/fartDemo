5774d9d50dbddedc59487cfc0e8d051edbc149be
arijitiiest <1999arijitchoudhury@gmail.com>
2020-12-13 00:15:22 +0530

    ...

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config({ path: path.join(__dirname, "utils", ".env") });
 const apiRoutes = require("./routes/api");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
